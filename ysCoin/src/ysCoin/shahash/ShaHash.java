f254ff5168750cca4adc876030319c5f7f4e6f4b
alexjoo3391 <61110626+alexjoo3391@users.noreply.github.com>
2021-10-27 20:24:55 +0900

    해쉬 이상하게 나오던거 해결

--- ysCoin/src/ysCoin/shahash/ShaHash.java
+++ ysCoin/src/ysCoin/shahash/ShaHash.java
@@ -5,28 +5,38 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class ShaHash {
-	
+
 	private String originalString;
-    private String hashedString;
+	private String hashedString;
 
-    public ShaHash(String originalString) {
-        this.originalString = originalString;
-        this.hashedString = hashingFunction(originalString);
-    }
+	public ShaHash(String originalString) {
+		this.originalString = originalString;
+		this.hashedString = hashingFunction(originalString);
+	}
 
-    private String hashingFunction(String originalString) {
-    	
-    	MessageDigest md;
-    	
+	private String hashingFunction(String originalString) {
+
+		MessageDigest md;
+
 		try {
 			md = MessageDigest.getInstance("SHA-256");
-	    	byte[] hash = md.digest(originalString.getBytes(StandardCharsets.UTF_8));
-	        return hash.toString();
+			byte[] hash = md.digest(originalString.getBytes(StandardCharsets.UTF_8));
+			return byteToHex(hash);
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 			return null;
 		}
-    }
+	}
+
+	public String byteToHex(byte[] bytes) {
+    	StringBuilder builder = new StringBuilder();
+    	
+    	for (byte data : bytes) {
+    		builder.append(String.format("%02x", data));
+		}
+    	
+    	return builder.toString();
+	}
 
 	public String getOriginalString() {
 		return originalString;
