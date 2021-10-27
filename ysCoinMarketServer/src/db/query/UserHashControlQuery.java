b5287935642a8b69c658e08a53f0ca243e0a9c56
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2021-10-27 11:40:22 +0900

    backup

--- ysCoinMarketServer/src/db/query/UserHashControlQuery.java
+++ ysCoinMarketServer/src/db/query/UserHashControlQuery.java
@@ -3,8 +3,6 @@ package db.query;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.cj.xdevapi.Result;
-
 public class UserHashControlQuery {
 	public int getUserHashCount(String userId, String coinname) {
 		return 0;
@@ -14,12 +12,16 @@ public class UserHashControlQuery {
 		return 0;
 	}
 	
-	public void hashOwnerTransfer(String userId, int price, int count, String type){
+	public void hashOwnerTransfer(String coinId, String userId, int price, int count, String type){
 		ResultSet rs = new UtilQuery().justGetResultSet("SELECT user_id, count FROM order_info WHERE price="+price+" AND order_type='"+type+"' ORDER BY order_time DESC");
 		try {
 			while(rs.next() && count > 0) {
-				rs.getString("user_id");
-				rs.getInt("count");
+				if(rs.getInt("count") <= count) {
+					count -= rs.getInt("count");
+					new UtilQuery().justUpdate("UPDATE hash SET user_id='"+userId+"' WHERE coin_id='"+coinId+"' AND user_id = '"+rs.getString("user_id")+"'");
+				} else {
+					
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
