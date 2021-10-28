775b0945cc408437016998dc082efb359192e759
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2021-10-28 09:54:01 +0900

    박건영이 수정해놓은거 다시 수정
    ㅇㅇ

--- ysCoinMarketServer/src/db/query/UserHashControlQuery.java
+++ ysCoinMarketServer/src/db/query/UserHashControlQuery.java
@@ -11,26 +11,15 @@ import db.JDBC;
 
 public class UserHashControlQuery {
 	public int getUserHashCount(String userId, String coinname) {
-		JDBC db = new JDBC("localhost", "yscoin", "root", "");
-	
-		Connection con = (Connection) db;
-		PreparedStatement pstmt = null;
-		String sql ="SELECT COUNT(hash) FROM `hash` WHERE user_id = ? AND coin_id = ?";
-		
-		try {
-			pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			pstmt.setString(1, userId);
-			pstmt.setString(2, coinname);
-		} catch (Exception e) {
-			
-		}
-		
-		return 0;
+		return (int) new UtilQuery().justGetObject(
+				"SELECT count(hash) FROM hash WHERE coin_id='"+coinname+"' AND user_id='"+userId+"'"
+		);
 	}
 
-	public int getUserOrderedHashCount(String userid, String coinname) {
-
-		return 0;
+	public int getUserOrderedHashCount(String userId, String coinname) {
+		return (int) new UtilQuery().justGetObject(
+				"SELECT sum(count) FROM order_info WHERE coin_id='"+coinname+"' AND user_id='"+userId+"' AND order_type='구매'"
+		);
 	}
 
 	public void hashOwnerTransfer(String coin_id, String userId, int price, int count, String type) {
