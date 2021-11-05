25191ad400876532ebf35ab8478f577208942817
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2021-11-05 11:11:14 +0900

    서버 - 거래내역 저장

--- ysCoinMarketServer/src/db/query/OrderQuery.java
+++ ysCoinMarketServer/src/db/query/OrderQuery.java
@@ -2,23 +2,26 @@ package db.query;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 
 import db.JDBC;
 
 public class OrderQuery {
-	public void buyAndSellRequest(String userId, String coinname, int price, int count, String type) {
-		int sellOrderCount = (int) new UtilQuery().justGetObject("SELECT sum(count) FROM order_info WHERE order_type = '"+ (type.equals("구매") ? "판매" : "구매")
-				+"' AND coin_id = '" + coinname + "'");
+	public void buyAndSellRequest(String userId, String coinId, int price, int count, String type) {
+		UtilQuery uq = new UtilQuery();
+		int sellOrderCount = (int) uq.justGetObject("SELECT sum(count) FROM order_info WHERE order_type = '"+ (type.equals("구매") ? "판매" : "구매")
+				+"' AND coin_id = '" + coinId + "'");
 
 		UserHashControlQuery uhcq = new UserHashControlQuery();
 		if(sellOrderCount == 0) {
-			addRequest(userId, price, count, type, coinname);
+			addRequest(userId, price, count, type, coinId);
 		} else if(sellOrderCount < count){
-			addRequest(userId, price, count-sellOrderCount, type, coinname);
-			uhcq.hashOwnerTransfer(coinname, userId, price, sellOrderCount, type);
+			addRequest(userId, price, count-sellOrderCount, type, coinId);
+			uhcq.hashOwnerTransfer(coinId, userId, price, sellOrderCount, type);
 		} else if(sellOrderCount >= count){
-			uhcq.hashOwnerTransfer(coinname, userId, price, count, type);
+			uhcq.hashOwnerTransfer(coinId, userId, price, count, type);
 		}
+		setTransactionDetails(uq, userId, coinId, count, sellOrderCount, price, type);
 	}
 	
 	public void addRequest(String userId, int price, int count, String type, String coinId) {
@@ -34,6 +37,11 @@ public class OrderQuery {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void setTransactionDetails(UtilQuery uq, String userId, String coinId, int orderingAmount, int penaltyAmount, int price, String orderType) {
+		uq.justUpdate("INSERT INTO `transaction_details`(`coin_id`, `ordering_amount`, `penalty_amount`, `price`, `order_type`, `user_id`) "+ 
+				"VALUES ('"+coinId+"','"+orderingAmount+"','"+penaltyAmount+"','"+price+"','"+orderType+"')");
 	}
 	
 	
