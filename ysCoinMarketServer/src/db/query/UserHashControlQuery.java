14b3f3f9f7215b28901070731dd258f7562aa0ff
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2021-11-05 10:35:42 +0900

    서버 - 거래수정3

--- ysCoinMarketServer/src/db/query/UserHashControlQuery.java
+++ ysCoinMarketServer/src/db/query/UserHashControlQuery.java
@@ -13,33 +13,52 @@ public class UserHashControlQuery {
 				+ "' AND user_id='" + userId + "' AND order_type='구매'");
 	}
 
-	public void hashOwnerTransfer(String coin_id, String userId, int price, int count, String type) {
+	public void hashOwnerTransfer(String coinId, String userId, int price, int count, String type) {
 		UtilQuery uq = new UtilQuery();
 		ResultSet rs = null;
 		if (type.equals("구매")) {
-			type = "판매";
 			rs = uq.justGetResultSet("SELECT user_id, count FROM order_info WHERE price<=" + price
-					+ " AND order_type='" + type + "' ORDER BY price order_time DESC");
+					+ " AND order_type='판매' ORDER BY price DESC, order_time DESC");
 		} else {
-			type = "구매";
 			rs = uq.justGetResultSet("SELECT user_id, count FROM order_info WHERE price>=" + price
-					+ " AND order_type='" + type + "' ORDER BY price order_time DESC");
+					+ " AND order_type='구매' ORDER BY price DESC, order_time DESC");
 		}
 		
 		try {
+			
+			int beforeCount = count;
+			
 			while (rs.next() && count > 0) {
-				if (rs.getInt(count) > count) {
-					uq.justUpdate("UPDATE hash SET user_id='" + userId + "' WHERE user_id='" + rs.getString("user_id")
-							+ "' AND coin_id='" + coin_id + " LIMIT " + count);
-					uq.justUpdate("UPDATE order_info SET count=count-"+count+" WHERE user_id='" + rs.getString("user_id")+"' price="+price + " ORDER BY order_time LIMIT 1");
+				String Buyer = "";
+				String Seller = "";
+				
+				if(type.equals("구매")) {
+					Buyer = userId;
+					Seller = rs.getString("user_id");
 				} else {
-					uq.justUpdate("UPDATE hash SET user_id='" + userId + "' WHERE user_id='" + rs.getString("user_id")
-							+ "' AND coin_id='" + coin_id + " LIMIT " + rs.getInt(count));
-					uq.justUpdate("DELETE FROM order_info WHERE user_id='" + rs.getString("user_id")+"' price="+price + " ORDER BY order_time LIMIT 1");
+					Buyer = rs.getString("user_id");
+					Seller = userId;
+				}
+				
+				if (rs.getInt(count) > count) {
+					uq.justUpdate("UPDATE hash SET user_id='" + Buyer + "' WHERE user_id='" + Seller
+					+ "' AND coin_id='" + coinId + " LIMIT " + count);
+					uq.justUpdate("UPDATE order_info SET count=count-"+count+" WHERE user_id='" + rs.getString("user_id") +"' price="+price + " coin_id='"+coinId+"' ORDER BY order_time LIMIT 1");
+					uq.justUpdate("UPDATE users SET money=money+"+(price*count)+" WHERE id='"+ Seller + "'");
+				} else {
+					uq.justUpdate("UPDATE hash SET user_id='" + Buyer + "' WHERE user_id='" + Seller
+							+ "' AND coin_id='" + coinId + " LIMIT " + rs.getInt(count));
+					uq.justUpdate("DELETE FROM order_info WHERE user_id='" + rs.getString("user_id") +"' price="+price + " coin_id='"+coinId+"' ORDER BY order_time LIMIT 1");
+					uq.justUpdate("UPDATE users SET money=money+"+(price*rs.getInt(count))+" WHERE id='"+ Seller + "'");
 				}
 
 				count -= rs.getInt(count);
 			}
 		} catch (Exception e) {}
+		
+	}
+	
+	public void updateTransactionDetails(String userId, String coinId, int count, int penaltyAmount) {
+		
 	}
 }
